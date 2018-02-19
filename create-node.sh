bada0842c99bf1408e4712831583a193868db559
David E Wexler <david.e.wexler@gmail.com>
2018-02-18 21:54:30 -0500

    Initial Commit

---
+++ create-node.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+ansible-playbook -K ansible/install-all.yml
+
+echo "Happy Mining! Your System is now mining"
